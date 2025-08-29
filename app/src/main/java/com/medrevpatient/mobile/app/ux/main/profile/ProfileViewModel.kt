1b6b27ee85fd2077612bf66d00257e94f4f447b0
imac <dixit.iroidsolutions@gmail.com>
2025-08-29 19:14:47 +0530

    BMI screen UI (done) diet screen UI (done)

--- app/src/main/java/com/medrevpatient/mobile/app/ux/main/profile/ProfileViewModel.kt
+++ app/src/main/java/com/medrevpatient/mobile/app/ux/main/profile/ProfileViewModel.kt
@@ -3,10 +3,8 @@ package com.medrevpatient.mobile.app.ux.main.profile
 import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.medrevpatient.mobile.app.navigation.DefaultNavBarConfig
-import com.medrevpatient.mobile.app.navigation.ViewModelNavBar
-import com.medrevpatient.mobile.app.navigation.ViewModelNavBarImpl
-import com.medrevpatient.mobile.app.ux.main.bottombar.NavBarItem
+import com.medrevpatient.mobile.app.navigation.ViewModelNav
+import com.medrevpatient.mobile.app.navigation.ViewModelNavImpl
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
@@ -15,11 +13,7 @@ import javax.inject.Inject
 class ProfileViewModel
 @Inject constructor(
     @ApplicationContext context: Context,
-    getSettingUiStateUseCase: GetProfileUiStateUseCase
-) : ViewModel(), ViewModelNavBar<NavBarItem> by ViewModelNavBarImpl(
-    NavBarItem.PROFILE, DefaultNavBarConfig(
-        NavBarItem.getNavBarItemRouteMap()
-    )
-) {
-    val uiState: ProfileUiState = getSettingUiStateUseCase(context = context, coroutineScope = viewModelScope) { navigate(it) }
+    getProfileUiStateUseCase: GetProfileUiStateUseCase
+) : ViewModel(), ViewModelNav by ViewModelNavImpl() {
+    val uiState: ProfileUiState = getProfileUiStateUseCase(context = context, coroutineScope = viewModelScope) { navigate(it) }
 }
